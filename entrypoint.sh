76e65269ffe8ba2a289e7102b4158e9cd8299dd4
Dmytro Titov <dmytrot@ifi.uio.no>
2019-09-02 11:08:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ sed -i 's!ELIXIR_AAI_CLIENT_SECRET!'${ELIXIR_AAI_CLIENT_SECRET}'!g' /etc/apache2
 sed -i 's!REDIRECT_URI!'${REDIRECT_URI:-"/oidc-protected"}'!g' /etc/apache2/sites-available/default-site.conf
 sed -i 's!CRYPTO_PASSPHRASE!'${CRYPTO_PASSPHRASE:-salt}'!g' /etc/apache2/sites-available/default-site.conf
 sed -i 's!RESPONSE_TYPE!'${RESPONSE_TYPE:-code}'!g' /etc/apache2/sites-available/default-site.conf
-sed -i 's!SCOPES!'${SCOPES}'!g' /etc/apache2/sites-available/default-site.conf
 sed -i 's!REMOTE_USER_CLAIM!'${REMOTE_USER_CLAIM:-sub}'!g' /etc/apache2/sites-available/default-site.conf
 sed -i 's!REQUEST_FIELD_SIZE!'${REQUEST_FIELD_SIZE:-65536}'!g' /etc/apache2/sites-available/default-site.conf
 
@@ -24,6 +23,7 @@ for PR in "${PRS[@]}"; do
 done
 
 # using perl instead of sed here, because sed fails miserably with new lines and slashes
+perl -i.bak -pe 's/SCOPES/$ENV{"SCOPES"}/g' /etc/apache2/sites-available/default-site.conf
 perl -i.bak -pe 's/LOCATIONS/$ENV{"LOCATIONS"}/g' /etc/apache2/sites-available/default-site.conf
 
 sed -i 's!SSL_ENGINE!'${SSL_ENGINE:-off}'!g' /etc/apache2/sites-available/default-site.conf
